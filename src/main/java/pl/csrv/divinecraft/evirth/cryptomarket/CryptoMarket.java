39de1633b2115231610d034ac0a72523eace9348
Bartosz Hliwa <bartosz.hliwa@gmail.com>
2018-03-13 22:26:11 +0100

    Check the version on enabling

--- src/main/java/pl/csrv/divinecraft/evirth/cryptomarket/CryptoMarket.java
+++ src/main/java/pl/csrv/divinecraft/evirth/cryptomarket/CryptoMarket.java
@@ -1,13 +1,20 @@
 package main.java.pl.csrv.divinecraft.evirth.cryptomarket;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import main.java.pl.csrv.divinecraft.evirth.cryptomarket.commands.CommandExecutorImpl;
 import main.java.pl.csrv.divinecraft.evirth.cryptomarket.models.Config;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.FileConfigurationOptions;
-import org.bukkit.plugin.java.JavaPlugin;
 import main.java.pl.csrv.divinecraft.evirth.cryptomarket.resources.ResourceManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Paths;
 
 public class CryptoMarket extends JavaPlugin {
@@ -17,6 +24,7 @@ public class CryptoMarket extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.checkVersion();
         pluginDir = Paths.get("plugins", CryptoMarket.class.getSimpleName()).toString();
         this.createFolders();
         this.initializeConfig();
@@ -50,5 +58,35 @@ public class CryptoMarket extends JavaPlugin {
         if (!resources.exists()) {
             resources.mkdirs();
         }
+    }
+
+    private void checkVersion() {
+        try {
+            URL url = new URL("https://api.github.com/repos/evirth/cryptomarket/releases/latest");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            br.lines().forEach(sb::append);
+            GitHubRepoVersion g = new ObjectMapper().readValue(sb.toString(), new TypeReference<GitHubRepoVersion>() {});
+            String version = this.getDescription().getVersion();
+            if (!g.getVersion().equals(version)) {
+                this.getLogger().info(String.format("New version (%s) is available: https://github.com/Evirth/CryptoMarket/releases", g.getVersion()));
+            }
+        } catch (Exception e) {
+            this.getLogger().warning(e.getMessage());
+        }
+    }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class GitHubRepoVersion {
+    @JsonProperty("tag_name")
+    private String version;
+
+    public String getVersion() {
+        if (this.version.startsWith("v")) {
+            return this.version.substring(1);
+        }
+        return this.version;
     }
 }
